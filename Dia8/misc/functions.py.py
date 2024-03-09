5b966697d993a237c7da0ddf77ac2ecd4d757c8a
Pedro Terra da Silva Motta <pterra.motta@gmail.com>
2024-03-09 19:17:11 -0300

    dia8

---
+++ Dia8/misc/functions.py.py
@@ -0,0 +1,17 @@
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Jack Bauer","Nowhere")  # positional arguments - default
+greet_with(location="Nowhere",name="Jack Bauer")  # keyword arguments - default
+
+
+
+
+
+
+
+
+
+#pylint: disable-all
